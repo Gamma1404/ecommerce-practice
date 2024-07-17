616ead69f240d3695dbe9ed385d2c54a16bc0937
Santiago Gama <131696138+Gamma1404@users.noreply.github.com>
2024-07-17 09:36:18 -0500

    feat(products): add GET /products endpoint

--- back/src/products/products.service.ts
+++ back/src/products/products.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    getProducts() {
+        return 'Retorna todos los Productos'
+    }
+}
